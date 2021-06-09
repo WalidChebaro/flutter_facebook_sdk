d8b8b01435ae3b45ace7cd3d369856b83873e4cd
Walid Chebaro <walidchebaro98@gmail.com>
2021-06-09 11:50:42 +0300

    Null Safety Migration

--- lib/flutter_facebook_sdk.dart
+++ lib/flutter_facebook_sdk.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 /// A singleton class for plugin
@@ -10,12 +9,12 @@ class FlutterFacebookSdk {
     if (_singleton == null) {
       _singleton = FlutterFacebookSdk._();
     }
-    return _singleton;
+    return _singleton!;
   }
 
   FlutterFacebookSdk._();
 
-  static FlutterFacebookSdk _singleton;
+  static FlutterFacebookSdk? _singleton;
 
   /// Method Channel Initilization to register method calls
   static const MethodChannel _channel =
@@ -25,7 +24,7 @@ class FlutterFacebookSdk {
   static const EventChannel _eventChannel =
       const EventChannel("flutter_facebook_sdk/eventChannel");
 
-  Stream<String> _onDeepLinkReceived;
+  Stream<String>? _onDeepLinkReceived;
 
   /// Returns a stream listener to handle deep link url changes
   /// Add a listener to this event to get updated deep link url
@@ -37,7 +36,7 @@ class FlutterFacebookSdk {
   /// });
   /// });
   /// ```
-  Stream<String> get onDeepLinkReceived {
+  Stream<String>? get onDeepLinkReceived {
     if (_onDeepLinkReceived == null) {
       _onDeepLinkReceived =
           _eventChannel.receiveBroadcastStream().cast<String>();
@@ -46,14 +45,14 @@ class FlutterFacebookSdk {
   }
 
   /// Returns the platform version of the running device
-  Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
   /// Returns deep link url
-  Future<String> get getDeepLinkUrl async {
-    final String url = await _channel.invokeMethod('getDeepLinkUrl');
+  Future<String?> get getDeepLinkUrl async {
+    final String? url = await _channel.invokeMethod('getDeepLinkUrl');
     return url;
   }
 
@@ -64,13 +63,13 @@ class FlutterFacebookSdk {
   }
 
   /// Logs View Content Event of FBSDK with [currency] and [price]
-  Future<bool> logViewedContent(
-      {@required String contentType,
-      @required String contentData,
-      @required String contentId,
-      @required String currency,
-      @required double price}) async {
-    final bool result = await _channel.invokeMethod("logViewedContent", {
+  Future<bool?> logViewedContent(
+      {required String contentType,
+      required String contentData,
+      required String contentId,
+      required String currency,
+      required double price}) async {
+    final bool? result = await _channel.invokeMethod("logViewedContent", {
       "contentType": contentType,
       "contentData": contentData,
       "contentId": contentId,
@@ -81,13 +80,13 @@ class FlutterFacebookSdk {
   }
 
   /// Logs Add to Cart Event of FBSDK with [currency] and [price]
-  Future<bool> logAddToCart(
-      {@required String contentType,
-      @required String contentData,
-      @required String contentId,
-      @required String currency,
-      @required double price}) async {
-    final bool result = await _channel.invokeMethod("logAddToCart", {
+  Future<bool?> logAddToCart(
+      {required String contentType,
+      required String contentData,
+      required String contentId,
+      required String currency,
+      required double price}) async {
+    final bool? result = await _channel.invokeMethod("logAddToCart", {
       "contentType": contentType,
       "contentData": contentData,
       "contentId": contentId,
@@ -98,13 +97,13 @@ class FlutterFacebookSdk {
   }
 
   /// Logs Add to Wishlist Event of FBSDK with [currency] and [price]
-  Future<bool> logAddToWishlist(
-      {@required String contentType,
-      @required String contentData,
-      @required String contentId,
-      @required String currency,
-      @required double price}) async {
-    final bool result = await _channel.invokeMethod("logAddToWishlist", {
+  Future<bool?> logAddToWishlist(
+      {required String contentType,
+      required String contentData,
+      required String contentId,
+      required String currency,
+      required double price}) async {
+    final bool? result = await _channel.invokeMethod("logAddToWishlist", {
       "contentType": contentType,
       "contentData": contentData,
       "contentId": contentId,
@@ -115,32 +114,33 @@ class FlutterFacebookSdk {
   }
 
   /// Logs Complete Registration Event of FBSDK with [registrationMethod]
-  Future<bool> logCompleteRegistration(
-      {@required String registrationMethod}) async {
-    final bool result = await _channel.invokeMethod("logCompleteRegistration", {
+  Future<bool?> logCompleteRegistration(
+      {required String registrationMethod}) async {
+    final bool? result =
+        await _channel.invokeMethod("logCompleteRegistration", {
       "registrationMethod": registrationMethod,
     });
     return result;
   }
 
   /// Logs Purchase Event of FBSDK with [currency] and [amount]
-  Future<bool> logPurhcase(
-      {@required double amount,
-      @required String currency,
-      @required Map<String, Object> params}) async {
-    final bool result = await _channel.invokeMethod("logPurchase",
+  Future<bool?> logPurhcase(
+      {required double amount,
+      required String currency,
+      required Map<String, Object> params}) async {
+    final bool? result = await _channel.invokeMethod("logPurchase",
         {"amount": amount, "currency": currency, "parameters": params});
     return result;
   }
 
   /// Logs Search Event of FBSDK with [searchString] and [success]
-  Future<bool> logSearch(
-      {@required String contentType,
-      @required String contentData,
-      @required String contentId,
-      @required String searchString,
-      @required bool success}) async {
-    final bool result = await _channel.invokeMethod("logSearch", {
+  Future<bool?> logSearch(
+      {required String contentType,
+      required String contentData,
+      required String contentId,
+      required String searchString,
+      required bool success}) async {
+    final bool? result = await _channel.invokeMethod("logSearch", {
       "contentType": contentType,
       "contentData": contentData,
       "contentId": contentId,
@@ -151,15 +151,15 @@ class FlutterFacebookSdk {
   }
 
   /// Logs Initiate Checkout Event of FBSDK with [numItems] and [paymentInfoAvailable]
-  Future<bool> logInitiateCheckout(
-      {@required String contentType,
-      @required String contentData,
-      @required String contentId,
-      @required int numItems,
-      @required bool paymentInfoAvailable,
-      @required String currency,
-      @required double totalPrice}) async {
-    final bool result = await _channel.invokeMethod("logInitiateCheckout", {
+  Future<bool?> logInitiateCheckout(
+      {required String contentType,
+      required String contentData,
+      required String contentId,
+      required int numItems,
+      required bool paymentInfoAvailable,
+      required String currency,
+      required double totalPrice}) async {
+    final bool? result = await _channel.invokeMethod("logInitiateCheckout", {
       "contentType": contentType,
       "contentData": contentData,
       "contentId": contentId,
@@ -183,11 +183,11 @@ class FlutterFacebookSdk {
   ///   valueToSum: 55
   /// ),
   /// ```
-  Future<bool> logEvent(
-      {@required String eventName,
-      double valueToSum,
+  Future<bool?> logEvent(
+      {required String eventName,
+      double? valueToSum,
       dynamic parameters}) async {
-    final bool result = await _channel.invokeMethod("logEvent", {
+    final bool? result = await _channel.invokeMethod("logEvent", {
       'eventName': eventName,
       'valueToSum': valueToSum,
       'parameters': parameters
@@ -198,8 +198,8 @@ class FlutterFacebookSdk {
   /// Only Available in iOS
   /// Set the advertiser tracking to truue or false
   /// App events won't work if this is disabled
-  Future<bool> setAdvertiserTracking({@required bool isEnabled}) async {
-    final bool result = await _channel
+  Future<bool?> setAdvertiserTracking({required bool isEnabled}) async {
+    final bool? result = await _channel
         .invokeMethod("setAdvertiserTracking", {"enabled": isEnabled});
     return result;
   }
